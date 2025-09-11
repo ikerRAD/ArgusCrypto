8ddacca9bcf34679451f2c8342baa3ed840aa235
IkerRAD <ikerbueno30@gmail.com>
2025-09-11 23:48:40 +0200

    feat(price_task) -> Created infrastructure for asyncronous tasks

---
+++ backend/app/tasks.py
@@ -0,0 +1,25 @@
+from celery import Celery
+from celery.utils.log import get_task_logger
+
+from app.settings import CELERY_BROKER_URL, CELERY_BACKEND_URL, BINANCE_INTERVAL
+
+celery_app = Celery(
+    "crypto-tasks",
+    broker=CELERY_BROKER_URL,  # Add your broker URL here
+    backend=CELERY_BACKEND_URL,  # Add your backend URL here
+)
+
+celery_app.conf.timezone = "UTC"
+
+celery_app.conf.beat_schedule = {
+    "fetch-binance-prices": {
+        "task": "app.tasks.fetch_and_store_binance_cripto_prices",
+        "schedule": BINANCE_INTERVAL,
+    }
+}
+
+logger = get_task_logger(__name__)
+
+@celery_app.task
+def fetch_and_store_binance_cripto_prices():
+    logger.info("Fetching prices...")
