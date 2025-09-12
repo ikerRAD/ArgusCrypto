44d3e6fe5349058634711503d771a3b0d761bcf4
IkerRAD <ikerbueno30@gmail.com>
2025-09-12 19:31:20 +0200

    feat(fastapi) -> created base api

---
+++ backend/app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+app = FastAPI()
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs", status_code=308)
